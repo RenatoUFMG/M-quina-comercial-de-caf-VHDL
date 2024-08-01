896f893da4ebfc883ebddb268b45e889aabcbb59
RenatoUFMG <132796454+RenatoUFMG@users.noreply.github.com>
2024-08-01 13:13:57 -0300

    Update Somador.vhd

--- TP/Somador.vhd
+++ TP/Somador.vhd
@@ -6,13 +6,14 @@ use ieee.numeric_std.all;
 entity Somador is
     generic
     (
-        DATA_WIDTH : natural:= 3
+        DATA_WIDTH : natural:= 4
     );
     port
     (
         clock: in std_logic;
         a: in std_logic_vector ((DATA_WIDTH-1) downto 0); 
         b: in std_logic_vector ((DATA_WIDTH-1) downto 0);
+        load: in std_logic;
         soma: out std_logic_vector ((DATA_WIDTH-1) downto 0);
 		reset : in std_logic
     );
@@ -23,7 +24,7 @@ begin
    begin
 		if (reset = '1') then
 			soma <= (others => '0');
-		elsif (rising_edge(clock)) then
+		elsif (rising_edge(clock) and load = '1') then
 			soma <= std_logic_vector(unsigned(a) + unsigned(b));
       end if;
     end process;
