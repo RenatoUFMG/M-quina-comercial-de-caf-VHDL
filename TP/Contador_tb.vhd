4fa1ed716dedc1719d41d295e2126ba624c371f1
RenatoUFMG <132796454+RenatoUFMG@users.noreply.github.com>
2024-08-01 13:09:55 -0300

    Update Contador_tb.vhd

--- TP/Contador_tb.vhd
+++ TP/Contador_tb.vhd
@@ -2,61 +2,34 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity Contador_tb is
-end Contador_tb;
+entity Contador is
+    port(
+        clock : in std_logic;
+        reset : in std_logic;
+        start : in std_logic;
+        tempo_in : in std_logic_vector(3 downto 0);
+        tempo_out : out std_logic
+    );
+end Contador;
 
-architecture tb of Contador_tb is
-    component Contador
-        port(
-            clock : in std_logic;
-            reset : in std_logic;
-            start : in std_logic;
-            tempo_in : in std_logic_vector(3 downto 0);
-            tempo_out : out std_logic_vector(3 downto 0)
-        );
-    end component;
-
-    signal clock_tb : std_logic := '0';
-    signal reset_tb : std_logic := '0';
-    signal start_tb : std_logic := '0';
-    signal tempo_in_tb : std_logic_vector(3 downto 0) := "0000";
-    signal tempo_out_tb : std_logic_vector(3 downto 0);
-
+architecture timer of Contador is
+    signal tempo : integer := 0;
 begin
-    uut : Contador
-        port map(
-            clock => clock_tb,
-            reset => reset_tb,
-            start => start_tb,
-            tempo_in => tempo_in_tb,
-            tempo_out => tempo_out_tb
-        );
-
-    clock_tb <= not clock_tb after 10 ns;
-
-    tb_proc : process
+    process(clock, reset)
     begin
-        -- Teste 1: Reset
-        reset_tb <= '1';
-        wait for 20 ns;
-        reset_tb <= '0';
-        wait for 20 ns;
-
-        -- Teste 2: Contagem
-        start_tb <= '1';
-        wait for 100 ns;
-        start_tb <= '0';
-        wait for 20 ns;
-
-        -- Teste 3: Entrada de tempo
-        tempo_in_tb <= "1010";
-        wait for 20 ns;
-        start_tb <= '1';
-        wait for 20 ns;
-        start_tb <= '0';
-        wait for 20 ns;
-
-        -- Fim do teste
-        wait;
+        if reset = '1' then
+            tempo <= 0;
+				tempo_out <= '0';
+        elsif rising_edge(clock) then
+            if start = '1' then
+                if tempo < unsigned(tempo_in) then
+                    tempo <= tempo + 1;
+                    tempo_out <= '0';
+                else
+                    tempo_out <= '1';
+                end if;
+            end if;
+        end if;
     end process;
-end tb;
+
+end timer;
